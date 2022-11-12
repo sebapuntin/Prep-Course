2e5fee571f14f6577732aa3b8349894e3ee3606a
sebapuntin <sebastianpuntin@gmail.com>
2022-11-12 17:15:49 -0300

    oki

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -34,8 +34,8 @@ function devolverString(str) {
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código
-  return x + y
+  // Tu código:
+  return (x + y)
 }
 
 function resta(x, y) {
